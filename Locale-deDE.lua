cd75fd8e28320e53102573e0e004067e00991899
xorann <xorann@gmail.com>
2018-08-14 18:00:29 +0200

    german triggers

--- Locale-deDE.lua
+++ Locale-deDE.lua
@@ -2,7 +2,11 @@ local L = AceLibrary("AceLocale-2.2"):new("Grim Reaper")
 
 L:RegisterTranslations("deDE", 
 	function()
-		return {	
+		return {
+			trigger_hit = "^(.+) trifft (.+) für ([%d])",
+			trigger_crit = "^(.+) trifft (.+) kritisch für ([%d])",
+			trigger_dot = "^([%w]+) erleidet ([%d]+) ([%w]+) von (.+) (durch (.+)).",
+			trigger_death = "^([%w]+) stirbt."
 		}
 	end
 )
